982197d0bd0a13aabca6c80e5893e56a6071dfe9
unknown <jh352160>
2017-12-13 14:40:37 +0800

    增加添加头尾布局功能

--- app/src/main/java/com/example/administrator/bindingrecycleradapter/BindingRecyclerAdapter.java
+++ app/src/main/java/com/example/administrator/bindingrecycleradapter/BindingRecyclerAdapter.java
@@ -18,12 +18,16 @@ import java.util.List;
  */
 
 public abstract class BindingRecyclerAdapter<T extends ViewDataBinding, M> extends RecyclerView.Adapter{
-    @LayoutRes
-    private int resId;
+    private final int TYPE_HEADER = 1;
+    private final int TYPE_FOOTER = 2;
 
     protected Context mContext;
     protected List<M> datas;
     protected OnItemClickListener<T,M> onItemClickListener;
+
+    @LayoutRes private int resId;
+    private View footerView;
+    private View headerView;
 
     /**
      * 空数据构造方法
@@ -41,12 +45,42 @@ public abstract class BindingRecyclerAdapter<T extends ViewDataBinding, M> exten
     protected abstract @LayoutRes int setLayoutResId();
 
     @Override
-    public BaseRecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public int getItemViewType(int position) {
+        if (headerView!=null&&(position==0)){
+            return TYPE_HEADER;
+        }
+        if (footerView!=null && (position == getItemCount()-1)){
+            return TYPE_FOOTER;
+        }
+        return super.getItemViewType(position);
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == TYPE_FOOTER){
+            return new HeaderAndFooterViewHolder(footerView);
+        }
+        if (viewType == TYPE_HEADER){
+            return new HeaderAndFooterViewHolder(headerView);
+        }
         return new BaseRecyclerViewHolder(parent, resId);
     }
 
     @Override
     public void onBindViewHolder(final RecyclerView.ViewHolder holder, int position) {
+        //头布局与尾布局不走接下来的方法
+        if (footerView!=null && (position == getItemCount()-1)){
+            return;
+        }
+        if (headerView!=null){
+            if (position==0) {
+                return;
+            }else{
+                //若存在头布局，position应该除去头布局，应为实际position-1
+                position--;
+            }
+        }
+
         final BaseRecyclerViewHolder baseHolder = (BaseRecyclerViewHolder) holder;
         onBindViewHolder(baseHolder.binding,datas.get(position), holder, position);
 
@@ -65,7 +99,11 @@ public abstract class BindingRecyclerAdapter<T extends ViewDataBinding, M> exten
 
     @Override
     public int getItemCount() {
-        return datas.size();
+        int itemCount = datas.size();
+        if (footerView!=null) itemCount++;
+        if (headerView!=null) itemCount++;
+
+        return itemCount;
     }
 
     /**
@@ -79,6 +117,29 @@ public abstract class BindingRecyclerAdapter<T extends ViewDataBinding, M> exten
                     viewGroup.getContext()), layoutId, viewGroup, false).getRoot());
             binding = DataBindingUtil.getBinding(this.itemView);
         }
+    }
+
+    /**
+     * adapter中header与footer的viewholder
+     */
+    class HeaderAndFooterViewHolder extends RecyclerView.ViewHolder {
+        HeaderAndFooterViewHolder(View view) {
+            super(view);
+        }
+    }
+
+    /**
+     * 添加尾布局
+     */
+    public void addFooter(View view){
+        footerView=view;
+    }
+
+    /**
+     * 添加头布局
+     */
+    public void addHeader(View view){
+        headerView=view;
     }
 
     public void setOnItemClickListener(OnItemClickListener<T,M> onItemClickListener) {
